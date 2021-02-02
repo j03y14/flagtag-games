cc0c6808ba9fa4c65667a9d335880a93e070ef9b
j03y14 <46087226+j03y14@users.noreply.github.com>
2021-02-02 14:37:04 +0900

    init: backend 프로젝트 초기 설정
    - 백엔드 프로젝트 초기 설정
    - express, nodemon, eslint, prettier 설정

---
+++ backend/src/app.js
@@ -0,0 +1,32 @@
+const createError = require('http-errors');
+const express = require('express');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
+require('dotenv').config();
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(cors({ origin: true, credentials: true }));
+
+// app.use('/', router);
+
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+app.use((err, req, res) => {
+  res.status(err.status || 500);
+  res.json(err.message);
+});
+
+const server = app.listen(3000, () => {
+  const { port } = server.address();
+
+  console.log('Server is working on port', port);
+});
