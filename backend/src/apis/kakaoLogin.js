5cfee099812d100d94f563baafb4333ab74de937
j03y14 <46087226+j03y14@users.noreply.github.com>
2021-02-03 19:26:22 +0900

    feat: 카카오 로그인을 하는데 필요한 api 모듈 추가
    카카오 로그인을 하는데 필요한 함수들을 모아둔 api 모듈을 추가했다.
    
    오류가 발생했을 때 어떤 단계에서 오류가 발생했는지를 알기 위해 catch 부분에서 에러 코드와 에러 메세지를 작성하고 throw했다. 이후에 상위 컨트롤러의 trycatch에서 다시 받아 next를 사용해 에러를 넘겨준다.

---
+++ backend/src/apis/kakaoLogin.js
@@ -0,0 +1,54 @@
+const axios = require('axios');
+const createError = require('http-errors');
+
+const { GET_SOCIAL_TOKEN_FAILED, GET_SOCIAL_USER_INFO_FAILED } = require('@constants/status');
+
+const kakaoSocialLoginHost = process.env.KAKAO_LOGIN_HOST;
+const kakaoUserInfoHost = process.env.KAKAO_GET_USER_INFO_HOST;
+const kakaoClientId = process.env.KAKAO_CLIENT_ID;
+const kakaoRedirectUrl = process.env.KAKAO_REDIRECT_URI;
+const kakaoClientSecret = process.env.KAKAO_CLIENT_SECRET;
+
+const kakaoLoginApi = {
+  async getAccessToken(code) {
+    const headers = {
+      'content-type': 'application/x-www-form-urlencoded;charset=utf-8',
+    };
+
+    try {
+      console.log(kakaoClientSecret);
+      const {
+        data,
+      } = await axios.post(
+        `${kakaoSocialLoginHost}/oauth/token?grant_type=authorization_code&client_id=${kakaoClientId}&redirect_uri=${kakaoRedirectUrl}&code=${code}&client_secret=${kakaoClientSecret}`,
+        null,
+        { headers },
+      );
+
+      return data.access_token;
+    } catch (error) {
+      console.error(error);
+      throw createError(GET_SOCIAL_TOKEN_FAILED, 'failed to get kakao access token');
+    }
+  },
+
+  async getUserInfo(accessToken) {
+    const headers = {
+      Authorization: `Bearer ${accessToken}`,
+      'content-type': 'application/x-www-form-urlencoded;charset=utf-8',
+    };
+
+    try {
+      const { data } = await axios.post(`${kakaoUserInfoHost}/v2/user/me`, null, {
+        headers,
+      });
+
+      return data.kakao_account;
+    } catch (error) {
+      console.error(error);
+      throw createError(GET_SOCIAL_USER_INFO_FAILED, 'failed to get kakao user info');
+    }
+  },
+};
+
+module.exports = kakaoLoginApi;
