5b562eef58103d46e2dca7211a745d4f6481aabb
j03y14 <46087226+j03y14@users.noreply.github.com>
2021-02-03 15:53:14 +0900

    feat: user controller 추가
    user와 관련된 api의 controller 역할을 하는 모듈을 추가했다.
    
    만약 필요한 데이터가 안 왔다면 에러를 발생시킨다.

---
+++ backend/src/routes/user/controller.js
@@ -0,0 +1,21 @@
+const createError = require('http-errors');
+
+const { DATA_REQUIRED } = require('@constants/status');
+const socialLogin = require('@services/socialLogin');
+
+const userController = {
+  socialLogin(req, res, next) {
+    const { social } = req.params;
+    const { code } = req.body;
+
+    try {
+      if (!social || !code) throw createError(DATA_REQUIRED, 'required data are not given');
+
+      socialLogin[social](code);
+    } catch (error) {
+      next(error);
+    }
+  },
+};
+
+export default userController;
