c36618b4579c6929ef10962a52c1939875231f38
Heon-Jun Lee <48055710+kkirico@users.noreply.github.com>
2021-02-03 15:01:21 +0900

    feat: 로그인 페이지 헤더 컴포넌트
    중복된 헤더에 재사용 가능하게 만들었습니다.

---
+++ frontend/src/components/reusable/header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import styled from 'styled-components';
+
+const FlagTagHeader = styled.div`
+  font-family: Major Mono Display;
+  margin: 40px;
+  font-size: 105px;
+  color: #000000;
+  display: flex;
+  justify-content: center;
+`;
+
+const Header = () => <FlagTagHeader className="header"> FLAGTAG GAMES </FlagTagHeader>;
+
+export default Header;
