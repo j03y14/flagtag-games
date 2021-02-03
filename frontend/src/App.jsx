09445e14b3bcc674ae18f5bbaed734a7d200e39d
Heon-Jun Lee <48055710+kkirico@users.noreply.github.com>
2021-02-03 15:32:58 +0900

    feat: 소셜로그인 리디렉팅 라우터 설정

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import SigninPage from './pages/SinginPage';
+import AuthCallback from './components/signin/AuthCallback';
 
 const App = () => {
   return (
     <Router>
       <Switch>
         <Route path="/signin" component={SigninPage} />
+        <Route path="/callback" component={AuthCallback} />
       </Switch>
     </Router>
   );
