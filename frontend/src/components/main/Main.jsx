47975d3f9d0e123b81720c91da905aaaa3b43eb6
Heon-Jun Lee <48055710+kkirico@users.noreply.github.com>
2021-02-04 10:13:33 +0900

    feat: 메인 페이지 컴포넌트

---
+++ frontend/src/components/main/Main.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import styled from 'styled-components';
+
+import Header from '../reusable/header';
+import CreateGame from './CreateGame';
+import PublicGameSearch from './PublicGameSearch';
+import PrivateGameEnter from './PrivateGameEnter';
+
+const MainWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export default function Signin() {
+  return (
+    <>
+      <Header />
+      <MainWrapper>
+        <CreateGame />
+        <PublicGameSearch />
+        <PrivateGameEnter />
+      </MainWrapper>
+    </>
+  );
+}
