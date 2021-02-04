3a1b97988024e91f1220d6d2f4b84a69a902867c
j03y14 <46087226+j03y14@users.noreply.github.com>
2021-02-05 00:11:49 +0900

    refactor: 카카오 로그인, 구글 로그인 버튼을 분리
    만약 로그인 버튼이 더 여러개가 생긴다면 Signin 컴포넌트에 여러 로그인 로직이 섞여 실제 로직의 복잡성보다 코드가 복잡하게 느껴질 것 같아서 분리하였다.

--- frontend/src/components/signin/Signin.jsx
+++ frontend/src/components/signin/Signin.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
-
 import styled from 'styled-components';
+
+import KaKaoSignInButton from './KakaoSignInButton';
+import GoogleSignInButton from './GoogleSignInButton';
 
 const SocialLogin = styled.div`
   display: block;
@@ -8,30 +10,12 @@ const SocialLogin = styled.div`
   left: 14%;
   top: 35%;
 `;
-const Button = styled.button`
-  margin: 10px;
-  padding: 5px 16px;
-  width: 100%;
-  border-radius: 6px;
-  border: 1px solid #e1e4e8;
-  font-size: 55px;
-  font-weight: 600;
-  background-color: #faea32;
-`;
-export default function Signin() {
-  const url = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REST_API_KEY}&redirect_uri=${process.env.REDIRECT_URI}&response_type=code`;
-  const clickLoginButton = () => {
-    window.location.href = url;
-  };
 
+export default function Signin() {
   return (
     <SocialLogin>
-      <Button type="button" onClick={clickLoginButton}>
-        카카오 로그인
-      </Button>
-      <Button type="button" onClick={clickLoginButton}>
-        구글 로그인
-      </Button>
+      <KaKaoSignInButton />
+      <GoogleSignInButton />
     </SocialLogin>
   );
 }
