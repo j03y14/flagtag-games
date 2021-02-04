f054200782a6d338f57ba5f0d0ff6f45882d1780
Heon-Jun Lee <48055710+kkirico@users.noreply.github.com>
2021-02-04 10:09:35 +0900

    feat: 메인 페이지
    라우팅 처리 및 페이지 컴포넌트 생성

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React from 'react';
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import SigninPage from './pages/SinginPage';
+// import AuthCallback from './components/signin/AuthCallback';
+import MainPage from './pages/MainPage';
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
-  )
-}
+    <Router>
+      <Switch>
+        <Route path="/signin" component={SigninPage} />
+        {/* <Route path="/callback" component={AuthCallback} /> */}
+        <Route path="/" component={MainPage} />
+      </Switch>
+    </Router>
+  );
+};
 export default App;
---
+++ frontend/src/pages/MainPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Main from '../components/main/Main';
+
+export default function MainPage() {
+  return (
+    <>
+      <Main />
+    </>
+  );
+}
