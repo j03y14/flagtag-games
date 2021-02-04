138c3653b85fd97bd2c39760ca9a92cc59ab12e9
Heon-Jun Lee <48055710+kkirico@users.noreply.github.com>
2021-02-04 18:00:47 +0900

    feat: 유저 모델 정의
    레벨의 필요에 따라 수정하였음
    index파일의 export 형식은 모델이 늘어나면 바꿔야함

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  password: String,
+  nickName: { type: String },
+  email: { type: String },
+  level: { type: Number },
+  social: { type: String },
 });
 module.exports = mongoose.model('User', userSchema);
