e4a2f31607a40ed5cc038e4b3f2e474bbdeeebec
Heon-Jun Lee <48055710+kkirico@users.noreply.github.com>
2021-02-04 10:11:42 +0900

    feat: 가운데 정렬 스타일 컴포넌트
    재사용을 위한 최소한의 스타일만 포함

---
+++ frontend/src/components/reusable/ColumnCenterDiv.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 50px;
+`;
+
+export default function ColumnCenterDiv() {
+  return <Wrapper />;
+}
