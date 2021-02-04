d018170955adcc64a87050f22a86494fb81e639d
j03y14 <46087226+j03y14@users.noreply.github.com>
2021-02-04 11:08:28 +0900

    feat: 사용자 정보와 이메일을 따로 반환하도록 수정
    email은 social과 함께 토큰을 만드는데 사용이 되고 userInfo는 body로 보내게 될 데이터라서 분리를 해주는 것이 편할 것이라고 생각해 분리했다.

--- backend/src/services/socialLogin/kakao.js
+++ backend/src/services/socialLogin/kakao.js
@@ -4,9 +4,12 @@ const kakaoLogin = async (code) => {
   try {
     const accessToken = await kakaoLoginApi.getAccessToken(code);
     const userInfo = await kakaoLoginApi.getUserInfo(accessToken);
-    console.log(userInfo);
 
-    // TODO: 사용자가 DB에 없으면 추가한 후에 사용자 정보로 토큰을 만들어 반환해준다.
+    // DB에서 사용자 정보를 가져오는 작업이 필요할듯
+    // level 같은 거는 우리 서비스의 DB에만 있으니까
+
+    // 반환할 때 userInfo라고 되어 있는거를 DB에서 가져온 정보로 바꿔주어야함.
+    return { email: userInfo.email, userInfo };
   } catch (error) {
     console.error(error);
     throw error;
@@ -14,3 +17,18 @@ const kakaoLogin = async (code) => {
 };
 
 module.exports = kakaoLogin;
+
+// userInfo 속의 정보 구조
+// {
+//   profile_needs_agreement: false,
+//   profile: {
+//     nickname: '박재윤',
+//     thumbnail_image_url: 'http://k.kakaocdn.net/dn/brRSGg/btqOuwjXyJA/54d1IXFxUwolt7UPvNeQk0/img_110x110.jpg',
+//     profile_image_url: 'http://k.kakaocdn.net/dn/brRSGg/btqOuwjXyJA/54d1IXFxUwolt7UPvNeQk0/img_640x640.jpg'
+//   },
+//   has_email: true,
+//   email_needs_agreement: false,
+//   is_email_valid: true,
+//   is_email_verified: true,
+//   email: 'j03y14@gmail.com'
+// }
