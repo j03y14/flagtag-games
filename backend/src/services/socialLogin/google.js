3740f9b0e02db1a887404d954e819cce4acb6e4a
j03y14 <46087226+j03y14@users.noreply.github.com>
2021-02-04 17:27:30 +0900

    chore: 구글 userinfo 구조 주석 추가
    구글 유저 info 정보를 추가했다. 이후에 배포 버전에서는 삭제를 해야한다.

--- backend/src/services/socialLogin/google.js
+++ backend/src/services/socialLogin/google.js
@@ -14,3 +14,15 @@ const googleLogin = async (code) => {
 };
 
 module.exports = googleLogin;
+
+// userinfo 정보
+// {
+//   "id": "113063555394098111226",
+//   "email": "j03y14@gmail.com",
+//   "verified_email": true,
+//   "name": "박재윤",
+//   "given_name": "재윤",
+//   "family_name": "박",
+//   "picture": "https://lh4.googleusercontent.com/-7ohF-8PAM5A/AAAAAAAAAAI/AAAAAAAAAAA/AMZuuclw4OYZlq7sSBodyFYQz38NIP0UZQ/s96-c/photo.jpg",
+//   "locale": "ko"
+// }
