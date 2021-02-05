61f9dc942452f51100de4b98dbe64a04959f8831
j03y14 <46087226+j03y14@users.noreply.github.com>
2021-02-05 15:50:29 +0900

    feat: 구글, 카카오 url 분리

--- frontend/src/components/signin/KakaoSignInButton.jsx
+++ frontend/src/components/signin/KakaoSignInButton.jsx
@@ -5,7 +5,7 @@ import SignInButton from '../reusable/SignInButton';
 const KaKaoSignInButton = () => {
   const host = process.env.KAKAO_OAUTH_HOST;
   const clientId = process.env.KAKAO_CLIENT_ID;
-  const redirectUri = process.env.REDIRECT_URI;
+  const redirectUri = `${process.env.REDIRECT_URI}/kakao`;
 
   const url = `${host}?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code`;
   const onKakaoSignInButtonClicked = () => {
