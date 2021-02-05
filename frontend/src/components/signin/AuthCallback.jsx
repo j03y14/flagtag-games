17fe9f7931fe9c0deb46955ec63d78a8c62acd3b
j03y14 <46087226+j03y14@users.noreply.github.com>
2021-02-05 15:49:58 +0900

    feat: 인증 api 추가
    인증 api 요청을 보낸 후 유저 정보를 local storage에 저장을 하는 것까지 완료가 되었다. 이후에 이것을 store에 저장하도록 변경해야할 것 같다.

--- frontend/src/components/signin/AuthCallback.jsx
+++ frontend/src/components/signin/AuthCallback.jsx
@@ -1,28 +1,26 @@
 import React, { useEffect } from 'react';
 import { useLocation, useHistory } from 'react-router-dom';
-import { oauthAPI } from '../../apis/auth';
+import oauthAPI from '../../apis/auth';
 
-export default function AuthCallback() {
+export default function AuthCallback({ social }) {
   const history = useHistory();
+  const location = useLocation();
 
   const getAccessToken = async (code) => {
-    const { accessToken, userInfo } = await oauthAPI.getAccessToken(code);
-
-    if (!accessToken) {
-      alert('액세스 토큰 없음');
-      return;
+    try {
+      const { userInfo } = await oauthAPI.getAccessToken(social, code);
+      window.localStorage.setItem('userInfo', JSON.stringify(userInfo));
+      history.push('/');
+    } catch (error) {
+      alert('로그인에 실패했습니다. 다시 시도해주세요');
+      history.push('/signin');
     }
-
-    window.localStorage.setItem('accessToken', accessToken);
-    window.localStorage.setItem('userInfo', JSON.stringify(userInfo));
-    history.push('/');
   };
 
-  const query = new URLSearchParams(useLocation().search);
-
   useEffect(() => {
+    const query = new URLSearchParams(location.search);
     getAccessToken(query.get('code'));
-  }, []);
+  }, [location, social]);
 
   return <div />;
 }
