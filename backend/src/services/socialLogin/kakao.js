e32148ff8bf88c6dab31b47d7548df89da1900e3
Heon-Jun Lee <48055710+kkirico@users.noreply.github.com>
2021-02-05 16:28:20 +0900

    feat: 소셜 로그인 유저 조회에 소셜 종류 추가

--- backend/src/services/socialLogin/kakao.js
+++ backend/src/services/socialLogin/kakao.js
@@ -5,7 +5,7 @@ const kakaoLogin = async (code) => {
   try {
     const accessToken = await kakaoLoginApi.getAccessToken(code);
     const userInfoFromKakao = await kakaoLoginApi.getUserInfo(accessToken);
-    const user = await userRepository.findByEmail(userInfoFromKakao.email);
+    const user = await userRepository.findByEmail(userInfoFromKakao.email, 'kakao');
     const defaultUserInfo = {
       email: userInfoFromKakao.email,
       nickName: userInfoFromKakao.profile.nickname,
