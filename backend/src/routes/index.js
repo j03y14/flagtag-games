7f77ea59d5264d59d68d7ec3d61201b73fa36718
j03y14 <46087226+j03y14@users.noreply.github.com>
2021-02-05 18:14:30 +0900

    feat: gameType router 추가

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,8 +1,10 @@
 const userRouter = require('@routes/user');
+const gameTypeRouter = require('@routes/gameType');
 const express = require('express');
 
 const rootRouter = express.Router();
 
 rootRouter.use('/api/user', userRouter);
+rootRouter.use('/api/gameTypes', gameTypeRouter);
 
 module.exports = rootRouter;
