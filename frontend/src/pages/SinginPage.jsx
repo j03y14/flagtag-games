f109badbb5aab56f3e5508e2f85689ebd1f28b20
Heon-Jun Lee <48055710+kkirico@users.noreply.github.com>
2021-02-06 00:53:21 +0900

    chore: 파일명 수정
    header라는 컴포넌트명에 혼동이 예상되어 flagtagHeader로 변경하고 그에따른 수정사항 반영함

--- frontend/src/pages/SinginPage.jsx
+++ frontend/src/pages/SinginPage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Redirect } from 'react-router-dom';
 
 import Signin from '../components/signin/Signin';
-import Header from '../components/reusable/header';
+import FlagTagHeader from '../components/reusable/FlagtagHeader';
 
 export default function SigninPage() {
   const token = localStorage.getItem('accessToken');
@@ -12,7 +12,7 @@ export default function SigninPage() {
 
   return (
     <>
-      <Header />
+      <FlagTagHeader />
       <Signin />
     </>
   );
