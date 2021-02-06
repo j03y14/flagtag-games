2c507af77a835622277f84c0d4f190e7915d3e96
j03y14 <46087226+j03y14@users.noreply.github.com>
2021-02-06 15:54:46 +0900

    chore: 절대경로 설정 추가
    webpack.config, jsconfig, eslinrc 파일로 절대경로 설정을 완료했다.

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -9,4 +9,18 @@ module.exports = {
     'no-console': 0,
     'react/prop-types': 0,
   },
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [
+          ['@', './src'],
+          ['@apis', './src/apis'],
+          ['@components', './src/components'],
+          ['@reusable', './src/components/reusable'],
+          ['@pages', './src/pages'],
+        ],
+        extensions: ['.js', '.jsx'],
+      },
+    },
+  },
 };
