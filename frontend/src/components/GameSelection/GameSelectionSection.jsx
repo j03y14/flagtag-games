59c537fc3e188678edfea55ec4f69493f6af87b6
j03y14 <46087226+j03y14@users.noreply.github.com>
2021-02-07 03:52:50 +0900

    refactor: 사용하지 않는 코드 삭제

--- frontend/src/components/GameSelection/GameSelectionSection.jsx
+++ frontend/src/components/GameSelection/GameSelectionSection.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 import GameSelectionItemContainer from '@components/GameSelection/GameSelectionItemContainer';
 import Section from '@/components/GameSelection/Section';
