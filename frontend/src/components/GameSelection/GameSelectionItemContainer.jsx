d14914a987ceb248cb25c807f5e55fc221f893a8
j03y14 <46087226+j03y14@users.noreply.github.com>
2021-02-07 03:37:07 +0900

    feat: GameSelectionItemContainer 생성
    GameItem을 담는 container를 생성했다. 가로로 담고 item들의 길이의 합이 container 길이보다 길면 overflow 해서 스크롤을 만든다.

---
+++ frontend/src/components/GameSelection/GameSelectionItemContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const GameSelectionItemContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 10px 10px;
+  width: 100%;
+  overflow-x: auto;
+`;
+
+export default GameSelectionItemContainer;
