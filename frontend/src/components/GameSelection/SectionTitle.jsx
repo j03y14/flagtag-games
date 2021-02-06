c4ef43f729e134a839d3c1432f45889c26b9f363
j03y14 <46087226+j03y14@users.noreply.github.com>
2021-02-07 03:33:35 +0900

    feat: section title 생성
    각 section의 title에 해당하는 컴포넌트를 생성했다.

---
+++ frontend/src/components/GameSelection/SectionTitle.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import styled from 'styled-components';
+
+const SectionTitleDiv = styled.div`
+  font-family: Major Mono Display;
+  margin-bottom: 40px;
+  font-size: 40px;
+  color: #000000;
+  display: flex;
+`;
+
+const SectionTitle = ({ title }) => <SectionTitleDiv> {title} </SectionTitleDiv>;
+
+export default SectionTitle;
