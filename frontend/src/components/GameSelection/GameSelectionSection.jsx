cf4b82f97a8c6175e3f15b02714e5c013a6b18df
j03y14 <46087226+j03y14@users.noreply.github.com>
2021-02-07 04:00:12 +0900

    feat: react lazy 사용해 게임 item lazy loading

--- frontend/src/components/GameSelection/GameSelectionSection.jsx
+++ frontend/src/components/GameSelection/GameSelectionSection.jsx
@@ -1,23 +1,24 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 
 import GameSelectionItemContainer from '@components/GameSelection/GameSelectionItemContainer';
 import Section from '@/components/GameSelection/Section';
-import GameItem from '@components/GameSelection/GameItem';
+const GameItem = lazy(() => import('@components/GameSelection/GameItem'));
 
 const GameSelectionSection = ({ gameList, selectedGame, setSelectedGame }) => {
   return (
     <Section title="게임선택">
       <GameSelectionItemContainer>
         {gameList.map(({ name, image, _id: id, rule }) => (
-          <GameItem
-            name={name}
-            image={image}
-            rule={rule}
-            key={id}
-            id={id}
-            selected={id === selectedGame.id}
-            setSelectedGame={setSelectedGame}
-          />
+          <Suspense key={id} fallback={<div>...loading</div>}>
+            <GameItem
+              name={name}
+              image={image}
+              rule={rule}
+              id={id}
+              selected={id === selectedGame.id}
+              setSelectedGame={setSelectedGame}
+            />
+          </Suspense>
         ))}
       </GameSelectionItemContainer>
     </Section>
