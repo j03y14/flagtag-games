a303b8076fb34101565dd369355a5595204ffdf2
j03y14 <46087226+j03y14@users.noreply.github.com>
2021-02-07 03:32:02 +0900

    feat: gameselection 라우터 설정

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SigninPage from '@pages/SinginPage';
 import AuthCallbackPage from '@pages/AuthCallbackPage';
 import MainPage from '@pages/MainPage';
+import GameSelectionPage from '@pages/GameSelectionPage';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <Switch>
         <Route path="/signin" component={SigninPage} />
         <Route path="/callback/:social" component={AuthCallbackPage} />
+        <Route path="/gameselection" component={GameSelectionPage} />
         <Route path="/" component={MainPage} />
       </Switch>
     </Router>
