e54a90d1ab1b39a43bde26923ea44a0264f32738
j03y14 <46087226+j03y14@users.noreply.github.com>
2021-02-08 14:25:10 +0900

    feat: submit 버튼 추가
    submit을 했을 때 현재 메인 페이지로 가게 되어있으나 나중에 게임 방 페이지로 변경해야한다.

--- frontend/src/pages/GameSelectionPage.jsx
+++ frontend/src/pages/GameSelectionPage.jsx
@@ -1,13 +1,45 @@
 import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { useHistory } from 'react-router-dom';
 
 import PageHeader from '@/components/GameSelection/PageHeader';
 import GameSelectionSection from '@components/GameSelection/GameSelectionSection';
 import GameRuleSection from '@components/GameSelection/GameRuleSection';
 import gameTypeApi from '@apis/gameType';
 
+const SubmitButton = styled.button`
+  width: 200px;
+  height: 100px;
+  font-size: 40px;
+  background: #5db075;
+  border-radius: 8px;
+  border: none;
+  font-weight: 700;
+  color: #ffffff;
+`;
+
+const GameSelectionPageDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+  position: relative;
+  width: 100%;
+`;
+
 const GameSelectionPage = () => {
   const [gameList, setGameList] = useState([]);
   const [selectedGame, setSelectedGame] = useState({});
+  const history = useHistory();
+
+  const onSubmitButtonClicked = () => {
+    if (!selectedGame.id) {
+      alert('게임을 선택해주세요.');
+      return;
+    }
+
+    history.push('/');
+  };
 
   useEffect(async () => {
     const gameTypes = await gameTypeApi.getAllGameTypes();
@@ -15,7 +47,7 @@ const GameSelectionPage = () => {
   }, []);
 
   return (
-    <div>
+    <GameSelectionPageDiv>
       <PageHeader />
 
       <GameSelectionSection
@@ -25,7 +57,9 @@ const GameSelectionPage = () => {
       />
 
       <GameRuleSection rule={selectedGame.rule} />
-    </div>
+
+      <SubmitButton onClick={onSubmitButtonClicked}>확인</SubmitButton>
+    </GameSelectionPageDiv>
   );
 };
 
