00960a60986b0339a61099d8258ec6a1e1934d11
j03y14 <46087226+j03y14@users.noreply.github.com>
2021-04-14 23:13:22 +0900

    chore: server uri .env에 추가
    server uri를 .env에 추가했습니다.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,5 +3,6 @@ module.exports = {
     KAKAO_OAUTH_HOST: process.env.KAKAO_OAUTH_HOST,
     KAKAO_CLIENT_ID: process.env.KAKAO_CLIENT_ID,
     REDIRECT_URI: process.env.REDIRECT_URI,
+    SERVER_URI: process.env.SERVER_URI,
   },
 };
